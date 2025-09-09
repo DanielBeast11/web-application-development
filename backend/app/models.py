f32f8e2561fdba676948d9707eca98d424e7104a
DanielBeast11 <daniel-ahmerov@mail.ru>
2025-09-09 09:55:50 +0300

    edit

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,3 +1,106 @@
-from django.db import models
+from minio import Minio
 
-# Create your models here.
+client = Minio(
+    "localhost:9000",
+    access_key="minio",
+    secret_key="minio124",
+    secure=False
+)
+
+def get_image_url(key: str) -> str:
+    return client.presigned_get_object("images", key)
+
+
+cars = [
+    {
+        'id': 1,
+        'name': 'GAZelle Business',
+        'license_plate': 'А911ВЕ77',
+        'VIN': 'XW8ZZZ61ZHG047860',
+        'mileage': 120000,
+        'price': 1200000,
+        'img_key': 'GAZelle1.jpeg',
+        'description': 'Легкий коммерческий грузовик GAZelle Business с дизельным двигателем Cummins ISF2.8'
+    },
+    {
+        'id': 2,
+        'name': 'Ford Transit',
+        'license_plate': 'А923ВЕ77',
+        'VIN': 'XW8ZZZ61ZHG457860',
+        'mileage': 100000,
+        'price': 2500000,
+        'img_key': 'FordTransit1.jpg',
+        'description': 'Среднетоннажный фургон Ford Transit с дизельным двигателем 2.2 TDCi'
+    },
+    {
+        'id': 3,
+        'name': 'Iveco Daily 50C15',
+        'license_plate': 'C999XЕ77',
+        'VIN': 'XW8ZZZ61ZHG023860',
+        'mileage': 170000,
+        'price': 1300000,
+        'img_key': 'Iveco1.jpg',
+        'description': 'Легкий грузовик Iveco Daily 50C15 с дизельным двигателем F1A'
+    },
+    {
+        'id': 4,
+        'name': 'GAZelle Next',
+        'license_plate': 'А902ВЕ77',
+        'VIN': 'XW8ZZZ61ZHG047123',
+        'mileage': 110000,
+        'price': 1400000,
+        'img_key': 'GAZelle2.jpg',
+        'description': 'Модернизированная версия GAZelle Next с бензиновым двигателем EvoTech 2.7'
+    },
+    {
+        'id': 5,
+        'name': 'Ford Transit',
+        'license_plate': 'B123ВЕ77',
+        'VIN': 'XW8ZZZ61ZGD457860',
+        'mileage': 150000,
+        'price': 2100000,
+        'img_key': 'FordTransit2.jpg',
+        'description': 'Ford Transit с высокой крышей и дизельным двигателем 2.0 EcoBlue'
+    },
+    {
+        'id': 6,
+        'name': 'Iveco Daily 70C15',
+        'license_plate': 'C100XЕ77',
+        'VIN': 'XW8ZZZ61ZHG047341',
+        'mileage': 165000,
+        'price': 1700000,
+        'img_key': 'Iveco2.png',
+        'description': 'Тяжелый грузовик Iveco Daily 70C15 с дизельным двигателем F1C'
+    }
+]
+
+order = {
+    "id": 1,
+    "date": "07.09.2025",
+    'status': "черновик",
+    "total_depreciation": 942000,
+    "cars": [
+        {
+            "id": 2,
+            "name": "Ford Transit",
+            "license_plate": "А923ВЕ77",
+            "VIN": "XW8ZZZ61ZHG457860",
+            "mileage": 100000,
+            "price": 2500000,
+            "img_key": "FordTransit1.jpg",
+            "comment": "Основной грузовик",
+            "depreciation": 500000
+        },
+        {
+            "id": 3,
+            "name": "Iveco Daily 50C15",
+            "license_plate": "C999XЕ77",
+            "VIN": "XW8ZZZ61ZHG023860",
+            "mileage": 170000,
+            "price": 1300000,
+            "img_key": "Iveco1.jpg",
+            "comment": "Резервный грузовик",
+            "depreciation": 442000
+        }
+    ],
+}
